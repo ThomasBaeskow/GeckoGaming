e48cf20e9ec9c837a7a3b29dbb71eadff82bdc2e
Sofia Dinatolo <92044969+cockatiella@users.noreply.github.com>
2022-12-01 13:30:26 +0100

    css fix

--- client/src/components/categories/Categories.js
+++ client/src/components/categories/Categories.js
@@ -1,21 +1,27 @@
-import "./categories.css"
-import React from 'react'
+import "./categories.css";
+import React from "react";
 
 function Categories() {
-   
   return (
     <div className="categoryContainer">
-      <hr/>
-        <h1 className="catH1">Categories:</h1>
-        <div className="categoryList">
-            <div className="brands">brands</div>
-            <div className="electronics">Electronics</div>
-            <div className="merchandising">Merchandising</div>
-            <div className="kids">Kids</div>
-        </div>
-        <button>More</button>
+      <hr className="hr-categories" />
+      <h1 className="catH1">Categories:</h1>
+      <div className="categoryList-left">
+        <div className="left pink">brands</div>
+        <div className="right orange">Electronics</div>
+      </div>
+      <div className="categoryList-right">
+       
+        <div className="right green">Merchandising</div>
+        <div className="left purple">Kids</div>
+      </div>
+
+    <div className="btn-container">
+    <button>More... ➡️</button>
     </div>
-  )
+      
+    </div>
+  );
 }
 
-export default Categories
+export default Categories;
