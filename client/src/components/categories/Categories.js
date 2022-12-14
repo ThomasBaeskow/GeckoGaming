9f6a5842c65d360cfbd7aadce5aee6acc5583bf6
anu <anupama.desikan@gmail.com>
2022-12-14 09:56:47 +0100

    some changes in products

--- client/src/components/categories/Categories.js
+++ client/src/components/categories/Categories.js
@@ -1,23 +1,23 @@
-
 import "./categories.css";
-import React from "react";
+import React, { useContext } from "react";
+import { MyContext } from "../../context/Context";
 
 function Categories() {
+  const { categoryList } = useContext(MyContext);
   return (
     <div className="categoryContainer">
       <hr className="hr-categories" />
       <h1 className="catH1">Categories:</h1>
       <div className="categoryList-left">
-        <div className="left pink">Brands</div>
-        <div className="right orange">Accesories</div>
+        <div className="left pink">{categoryList[0]}</div>
+        <div className="right orange">{categoryList[1]}</div>
       </div>
       <div className="categoryList-right">
-       
-        <div className="right green">Computer</div>
-        <div className="left purple">Videogames</div>
+        <div className="right green">{categoryList[2]}</div>
+        <div className="left purple">{categoryList[3]}</div>
       </div>
 
-    {/* <div className="btn-container">
+      {/* <div className="btn-container">
     <button>More... ➡️</button>
     </div> */}
       <hr className="hr-categories" />
@@ -26,4 +26,3 @@ function Categories() {
 }
 
 export default Categories;
-
