2a5fe4ef867d2c90f4fc424469bd3d1a9451f59b
chaosnoneko <jhristonova@gmail.com>
2023-01-04 17:05:59 +0200

    button

--- client/src/components/myAccount/MyAccount.js
+++ client/src/components/myAccount/MyAccount.js
@@ -1,5 +1,5 @@
 import "./myAccount.css";
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, uploadUserPhoto, setFieldValue } from "react";
 import { useLocation, useNavigate, NavLink } from "react-router-dom";
 import image from "../../images/profile-pic.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -99,7 +99,8 @@ function MyAccount() {
           <p className="userName"> Hi,{userData.user.name}</p>
 
           <div className="myAccountImg">
-            <FontAwesomeIcon className="editIcon" icon={faEdit} />
+         <button className="upload"><FontAwesomeIcon className="editIcon" icon={faEdit} /></button>
+              
 
             <img src={image} alt="" className="img-profile" />
           </div>
@@ -155,3 +156,6 @@ function MyAccount() {
 }
 
 export default MyAccount;
+
+
+
