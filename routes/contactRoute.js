34ff1bb848d3b08759fcbfe87b0fe68b3d357db0
OVDCI <oraziovalerio.ventura@dci-student.de>
2023-01-10 15:01:59 +0100

    contact form working

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -1,20 +1,20 @@
 import express  from "express";
 import sgMail from "@sendgrid/mail"
-import path from "path";
+// import path from "path";
 
 const app = express();
 
-app.route("/").get(function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/src/components/aboutUs/AboutUs.js'));
- });
+// app.route("/").get(function (req, res) {
+//     res.sendFile(path.join(__dirname, '../client/src/components/aboutUs/AboutUs.js'));
+//  });
 
 app.post('/contact', (req, res) => {
-    // console.log(req);
+     console.log(req.body);
     const msg = {
         to: `thomasbaeskow@web.de`, // Change to your recipient
-        from: `baeskowt@gmail.com`, // Change to your verified sender
+        from: `${req.body.from}`, // Change to your verified sender
          subject: "inquire",
-        text: `Message from ${req.body.email}:\n${req.body.message}`,
+        text: `Message from ${req.body.from}:\n${req.body.text}`,
     }
     try {
         sgMail.send(msg);
